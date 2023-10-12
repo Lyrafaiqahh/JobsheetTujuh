7e35ab96846b74d7837948723f3d926416687018
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-10-12 13:39:25 +0700

    PERCOBAAN 1 JAWABAN PERTANYAAN NO.4 JOBSHEET 7

--- WhileKelipatan15.java
+++ WhileKelipatan15.java
@@ -1,29 +1,24 @@
 import java.util.Scanner;
-public class ForKelipatan15 {
 
+public class WhileKelipatan15 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int i = 1;
         int kelipatan;
         int jumlah = 0;
         int counter = 0;
-        float rata;
-        
-
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
 
-        for (int i = 1; i <= 50; i++) {
+        while (i <=50) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
-
                 System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
                 System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
 
-                rata = jumlah /  i;
-                System.out.println("rata - rata: " +rata);
-            }
-        }
+        } 
+        i++;
     }
+  }
 }
